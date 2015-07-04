a581acd9744cb2d33363f9754a04bfcd379b2416
Kelly Lauren-Summer Becker-Neuding <kbecker@kellybecker.me>
2015-07-04 02:05:54 -0700

    Changed to build an image for the Soekris net6501

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# RPI2 Toolchain Debian Jessie
+# net6501 Toolchain Debian Sid
 
-This toolchain is used to create a Debian Jessie Raspberry Pi 2 boot image.
+This toolchain is used to create a Debian Sid Soekris net6501 boot image.
 
 ## Dependencies
 
@@ -8,7 +8,7 @@ Dependencies are listed at http://github.com/KellyLSB/mkdebdisk.img
 
 ## Usage
 
-    $ git clone git://github.com/KellyLSB/rpi2
-    $ cd rpi2
+    $ git clone git://github.com/KellyLSB/mkdebdisk.img-soekris-net6501.git
+    $ cd mkdebdisk.img-soekris-net6501
     $ git submodule update --checkout --init
     $ ./build.sh
--- build.sh
+++ build.sh
@@ -1,59 +1,10 @@
 #!/bin/bash
 
-ARCH="armhf"
-DIST="jessie"
+ARCH="amd64"
+DIST="sid"
 
 # Load in library
 source mkdebdisk.img/mkdebdisk.img.lib.bash
-
-# RPI Firmware Files to Install
-# (from github)
-RPI_FIRMWARE_FILES=(
-	"README.md"
-	"COPYING.linux"
-	"LICENSE.broadcom"
-	"bcm2708-rpi-cm.dtb"
-	"bcm2708-rpi-b.dtb"
-	"bcm2708-rpi-b-plus.dtb"
-	"bcm2708-rpi-2-b.dtb"
-	"bootcode.bin"
-	"fixup_cd.dat"
-	"fixup_db.dat"
-	"fixup_x.dat"
-	"start.elf"
-	"start_cd.elf"
-	"start_db.elf"
-	"start_x.elf"
-)
-
-# RPi2Firmware installation
-# Copies the kernel to the expected kernel name by the Pi.
-# Downloads the non-free firmwares and device tree blobs from GitHub.
-# Installs the cmdline.txt and config.txt to the boot partition.
-function RPi2Firmware() {
-	Script "Rpi2KernelInitd" <<-EOF
-	cp "\$(ls /boot/vmlinuz* | sort | tail -n1)" /boot/kernel7.img
-	cp "\$(ls /boot/initrd* | sort | tail -n1)" /boot/initrd.img
-	cp "\$(ls /boot/System.map* | sort | tail -n1)" /boot/Module7.symvers
-	EOF
-
-	for file in "${RPI_FIRMWARE_FILES[@]}"; do
-		url="https://github.com/Hexxeh/rpi-firmware/blob/master/${file}?raw=true"
-
-		Script "Rpi2Firmware" <<-EOF
-		echo "Fetching: $(basename "${file}")"
-		curl -#Lk "${url}" > "/boot/${file}"
-		EOF
-	done
-
-	File "RPi2CmdLine" "/boot/cmdline.txt" <<-EOF
-	$(cat "${PWD}/config/cmdline.txt")
-	EOF
-
-	File "RPi2Config" "/boot/config.txt" <<-EOF
-	$(cat "${PWD}/config/config.txt")
-	EOF
-}
 
 #                          #
 # Begin Debdisk definition #
@@ -86,13 +37,13 @@ SystemdStartService \
 # Apt Repositories
 #
 
-AptRepo "httpredir.debian.org/debian" "jessie" \
+AptRepo "httpredir.debian.org/debian" "sid" \
 	main contrib non-free
 
-AptRepo "http.us.debian.org/debian" "jessie" \
+AptRepo "http.us.debian.org/debian" "sid" \
 	main contrib non-free
 
-AptRepo "ftp.us.debian.org/debian" "jessie" \
+AptRepo "ftp.us.debian.org/debian" "sid" \
 	main contrib non-free
 
 AptRepoSources \
@@ -102,29 +53,7 @@ AptInstall <<-EOF >> ${CUSTOMIZE}
 	iptables iproute2 iproute2-doc
 	openssh-server openssh-client mosh
 	git-core binutils kmod
-EOF
-
-### --- RPi2 Specific --- ###
-
-AptKeyFile ${PWD}/config/raspbian-archive-keyring.gpg
-AptKeyFile ${PWD}/config/collabora-archive-keyring.gpg
-
-AptKeysImport \
-	>> ${CUSTOMIZE}
-
-AptRepo "archive.raspbian.org/raspbian" "jessie" \
-	main contrib non-free firmware rpi
-
-AptRepo "repositories.collabora.co.uk/debian"	"jessie" \
-	rpi2
-
-AptRepoSources \
-	>> ${CUSTOMIZE}
-
-AptInstall <<-EOF >> ${CUSTOMIZE}
-linux-image-3.18.0-trunk-rpi2
-linux-headers-3.18.0-trunk-rpi2
-linux-support-3.18.0-trunk
+	linux-base grub-pc
 EOF
 
 AptCleanup \
@@ -133,10 +62,7 @@ AptCleanup \
 AptRepoSources no-proxy \
 	>> ${CUSTOMIZE}
 
-SystemdRootPassword "pi" \
-	>> ${CUSTOMIZE}
-
-RPi2Firmware \
+SystemdRootPassword "net6501" \
 	>> ${CUSTOMIZE}
 
 chmod +x ${CUSTOMIZE}
@@ -145,7 +71,7 @@ sudo vmdebootstrap \
 	--arch "${MKDDP_ARCH}" \
 	--distribution "${MKDDP_DIST}" \
 	--mirror "http://${APT_PROXY}httpredir.debian.org/debian" \
-	--image "rpi2-${MKDDP_DIST}-${MKDDP_TIME}.img" \
+	--image "net6501-${MKDDP_DIST}-${MKDDP_TIME}.img" \
 	--size 2048M \
 	--bootsize 64M \
 	--boottype vfat \
@@ -153,10 +79,10 @@ sudo vmdebootstrap \
 	--verbose \
 	--no-kernel \
 	--no-extlinux \
-	--hostname rpi2 \
+	--hostname net6501 \
 	--foreign "$(which qemu-arm-static)" \
 	--customize "${PWD}/${CUSTOMIZE}" \
-	--serial-console-command "/sbin/getty -L ttyAMA0 115200 vt100" \
+	--serial-console-command "/sbin/getty -L ttyS0  115200 vt100" \
 	--package debian-archive-keyring \
 	--package apt-transport-https \
 	--package debootstrap \
